05f6eca1f35524a9dbb6eff12e27c3cac6a03398
Bhaloo-bit <b604363@gmail.com>
2026-08-12 17:31:13 +0000

    singlyll update

--- DSA/Practice/01_practice.py
+++ DSA/Practice/01_practice.py
@@ -21,7 +21,20 @@ class Singllylinkedlist :
         temp = Node(value)
         temp.next = self.head 
         self.head = temp
-
+    def deleteLL(self, value):
+            t1 = self.head
+            prev = t1 
+            if(t1.data == value):
+                self.head = t1.next
+            while (t1.next != None):
+                if(t1.data == value):
+                    prev.next = t1.next
+                    break
+                else:
+                    prev = t1
+                    t1 = t1.next
+            if(t1.data == value):
+                prev.next = None
 
     def printLL (self):
         t1 = self.head 
