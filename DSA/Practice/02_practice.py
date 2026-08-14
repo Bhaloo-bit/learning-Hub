f5b7e5376203fc324a4706cfa8db76558eb7029c
Bhaloo-bit <b604363@gmail.com>
2026-08-14 17:32:06 +0000

    prac 2 DDL

--- DSA/Practice/02_practice.py
+++ DSA/Practice/02_practice.py
@@ -21,3 +21,38 @@ class DoublyLL :
 
         t.next = temp
         t.prev = t
+    def insertAtBegn (self, value):
+            temp = Node(value)
+            if(self.head ==None):
+                self.head = temp
+                return
+            temp.next = self.head
+            self.head.prev = temp
+            self.head = temp
+    
+    def insertAtMid (self, value,x):
+        t = self.head
+        
+        while (t.next != None):
+            if(t.value == x):
+                break
+            else:
+                t = t.next
+        temp = Node(value)
+        temp.next = t.next
+        t.prev = temp
+        t.next = temp
+        temp.prev = t
+
+
+    def printLL(self):
+            t1 = self.head
+            while(t1.next != None):
+                print(t1.value,end=" <--> ")
+                t1 = t1.next
+            print(t1.value)    
+
+Obj = DoublyLL()
+Obj.insertAtEnd(10)
+Obj.insertAtEnd(20)
+Obj.insertAtEnd(30)
