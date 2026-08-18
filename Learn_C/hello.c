3bd418445c477421072dad52473625ee53e072f9
Bhaloo-bit <b604363@gmail.com>
2026-08-18 17:13:57 +0000

    lession 1

--- Learn_C/hello.c
+++ Learn_C/hello.c
@@ -1,6 +1,33 @@
-# include <stdio.h>
+/*#include<stdio.h>
+int main(){
+    int x;
+    int y;
+    
+    printf("Enter the value of x:");
+    scanf("%d",&x);
+    printf("Enter the value of y:");
+    scanf("%d",&y);
+    
+    int result = x * y;
+    printf("result %d", result);
+
+    return 0;
+}*/
+
+// if else in C
+
+#include<stdio.h>
 int main (){
-    printf("!Hello World \n");
+    int age;
+    printf("Enter your age : ");
+    scanf("&d",age);
+
+    if(age < 18){
+        printf("notable to vote");
+    }
+    else {
+        printf("able to vote \n");
+    }
+    printf("thankyou \n");
     return 0;
 }
-
