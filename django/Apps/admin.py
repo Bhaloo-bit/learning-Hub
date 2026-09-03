361fb7bec32e28e175ae80f08007d8e49f48071b
Bipul Nirala <b604363@gmail.com>
2026-09-03 23:42:26 +0530

    django relationships models (DB ref)

--- django/Apps/admin.py
+++ django/Apps/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import ChaiVarity
+from .models import ChaiVarity, ChaiReview, ChaiCertificate,Store
 
 # Register your models here.
+class ChaiReviewInline(admin.TabularInline):
+    model= ChaiReview
+    extra = 2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display =('name','type','date_added')     
+
+
 admin.site.register(ChaiVarity)
+
