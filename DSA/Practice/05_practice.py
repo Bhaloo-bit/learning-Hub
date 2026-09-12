d84da2e60c1fc1894664374e7b75f1e3b86a4452
Bipul Nirala <b604363@gmail.com>
2026-09-13 00:29:11 +0530

    django restful api

---
+++ DSA/Practice/05_practice.py
@@ -0,0 +1,24 @@
+'''name = "bipul kumar nirala"
+#result = name.split()
+#result = name.strip()
+result = name.find("n")
+result = name.lower() 
+result = name.upper()  
+result = name.title()  
+result = name.title()  
+
+
+print(result)'''
+
+dict = {
+    'first_name': "Bipul ",
+    'mid_name': "Kumar ",
+    'last_name': "Nirala",
+    'salary' : 68669
+}
+
+print(dict.keys())
+print(dict.values())
+dict['first_name'] ="Bhumi"
+print(dict)
+print(dict.popitem())
